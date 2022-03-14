095913c0c17afd33b65647c0ed506ba56e05cde2
cee-tech21 <easyoandi@gmail.com>
2022-03-14 15:21:13 +0100

    Add overflow-scroll class to cash history div

--- assets/dom_man.js
+++ assets/dom_man.js
@@ -3,11 +3,13 @@ let enteredAmountEl = $("#entered-amount")
 let expOrIncomeEl = $("#exp-inc")
 let submitButtonEl = $("#submit-btn")
 let viewTransHistory = $("#trans-history")
+let transHistDiv = $("#tranz-hist")
 let currentBalEl = $("#current-bal")
 
 // Declare some variables
 let theTotal = 0;
 let cashHistory = "";
+let transHist = "";
 
 // Manipulate DOM
 submitButtonEl.click(function(){
@@ -23,8 +25,11 @@ submitButtonEl.click(function(){
             theTotal -= enteredAmountEl.val()
             currentBalEl.text( theTotal )
             cashHistory = `<option>${expOrInc} - ${enteredAmountEl.val()} </option>`
+
+            transHist = `<p class="bg-light py-2 px-2">
+                    ${expOrInc} - ${enteredAmountEl.val()} </p>`
             viewTransHistory.append(cashHistory)
-            
+            transHistDiv.append(transHist)
 
         } else if (expOrInc === "Income"){
             theTotal += Number(enteredAmountEl.val())
@@ -32,6 +37,10 @@ submitButtonEl.click(function(){
             cashHistory = 
                 `<option>${expOrInc} + ${enteredAmountEl.val()} </option>`
             viewTransHistory.append(cashHistory)
+            transHist = `<p class="bg-light py-2 px-2">
+                    ${expOrInc} - ${enteredAmountEl.val()} </p>`
+            transHistDiv.append(transHist)
+
 
         }
     } 
